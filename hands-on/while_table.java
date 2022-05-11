50324dcba34be1b3e85b5e5f948019d290a9c01a
Suraj-Suryawanshi <74811909+Suraj-Suryawanshi@users.noreply.github.com>
2022-05-11 12:58:27 +0530

    Add files via upload

---
+++ hands-on/while_table.java
@@ -0,0 +1,22 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class while_table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number to print table: ");
+		int num=sc.nextInt();
+		int i=1;
+		
+		//while loop incrementing value from 1 to 10
+		while(i<=10) {
+			System.out.println(num+" x "+i+" x "+i*num);
+			i++;
+		}
+	}
+
+}
