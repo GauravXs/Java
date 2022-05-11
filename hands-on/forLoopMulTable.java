20bf1bca50bfe15c8ee41dd45c94cb2454f6fc07
Suraj-Suryawanshi <74811909+Suraj-Suryawanshi@users.noreply.github.com>
2022-05-11 12:13:57 +0530

    Add files via upload

---
+++ hands-on/forLoopMulTable.java
@@ -0,0 +1,17 @@
+package corejava;
+
+import java.util.Iterator;
+
+public class forLoopMulTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=2;
+		
+		for(int i=1;i<=11;i++) {
+			System.out.println("2 x "+i+" x "+i*2);
+		}
+
+	}
+
+}
