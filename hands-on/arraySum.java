0226d46770c8be4662436eb9e9d878f5b8a8cc0b
Suraj-Suryawanshi <74811909+Suraj-Suryawanshi@users.noreply.github.com>
2022-05-23 11:04:05 +0530

    Add files via upload

---
+++ hands-on/arraySum.java
@@ -0,0 +1,30 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class arraySum {
+
+	public static void main(String[] args) {
+		   int len;  //it will define the length if array
+		   Scanner sc = new Scanner(System.in);
+		   System.out.println("Enter Array length: ");
+		   len = sc.nextInt();
+		   
+		   int arr[]= new int[len];
+		   System.out.println("Enter "+ len + "Element to store in Array\n" );
+		   
+		   for(int i=0; i<len; i++) 
+		   {
+		     arr[i]=sc.nextInt();
+		   }
+		   
+		   int sum=0;   
+		   //for calculating sum 
+		   for(int i=0;i<len;i++) {
+			   sum+=arr[i];
+		   }
+		   System.out.println("The sum of array :"+sum);
+
+	}
+
+}
