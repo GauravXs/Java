1f773fe129ba53cdf432dc40e429eab61d2cad60
Suraj-Suryawanshi <74811909+Suraj-Suryawanshi@users.noreply.github.com>
2022-05-24 12:25:20 +0530

    Add files via upload

---
+++ Practical/arraySort.java
@@ -0,0 +1,30 @@
+package corejava;
+//Java Program to Sort Array of Integers
+//by Default Sorts in an Ascending Order
+//using Arrays.sort() Method
+
+//Importing Arrays class from the utility class
+import java.util.Arrays;
+//Main class                                            
+public class arraySort {
+
+
+
+  // Main driver method
+  public static void main(String[] args)
+  {
+      // Custom input array
+      int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 }; // SD Array
+
+
+
+      // Applying sort() method over to above array
+      // by passing the array as an argument
+      Arrays.sort(arr);
+
+
+
+      // Printing the array after sorting
+      System.out.printf("Modified arr[] : %s your answer", Arrays.toString(arr));
+  }
+}
