c80beff1aafdf5ec883b0b29e0c106da381ecec0
Suraj-Suryawanshi <74811909+Suraj-Suryawanshi@users.noreply.github.com>
2022-05-30 12:25:25 +0530

    Add files via upload

---
+++ Lab-Sessions/Assignment-26-05-2022/LinkedListIterate.java
@@ -0,0 +1,29 @@
+package LabSessions;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class LinkedListIterate {
+	public static void main(String[] args) {
+		//Creating instance of Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		LinkedList<String> al=new LinkedList<String>();  //Creating LinkedList
+		int n=5;	//Declaring and Initialization of variable n 
+		
+		System.out.println("Enter 5 values here: ");
+		for(int i=0;i<n;i++) {		//here n is the length of an linkedList
+			al.add(sc.next());		//Taking input from user
+		}
+		sc.close();
+		
+		System.out.println("Iterating the Elements of Linked LIst:");
+		//Traversing list through iterator
+		Iterator<String> itr=al.iterator();  
+		while(itr.hasNext()){  
+		System.out.print(" "+itr.next());  
+		}   	
+	}
+
+}
---
+++ Lab-Sessions/Assignment-26-05-2022/RemoveArrayElement.java
@@ -0,0 +1,42 @@
+//Java Program to remove a particular element in a an array
+
+package LabSessions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class RemoveArrayElement {
+	
+	public static void main(String[] args) {
+		//Creating object of Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		 int[] my_array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49}; //Declaring and initializing an array
+		 System.out.print("Original Array: ");
+		 //For printing all the array elements
+		 for(int i=0;i<my_array.length;i++) {
+			 System.out.print(" "+my_array[i]);
+		 }
+		 
+		 System.out.println("\n Enter Index of an element you want to remove: ");
+		 int removeIndex = sc.nextInt();	//Taking input from the user 
+		 int j=0;		//Declared and Initialized variable j
+		 for(int i = 0; i < my_array.length -1; i++)
+		 {
+			//Value of i is the index we want to remove i.e removeIndex value
+			if(i==removeIndex)
+		    {
+		    	continue;
+		    }
+		    else
+		     {
+		        my_array[j] = my_array[i];
+		        j++;
+		     }
+		    
+		    }
+		     System.out.println("New Array : "+Arrays.toString(my_array));  //for converting Array toString & printing new array 
+		    }
+		  }
+
+
