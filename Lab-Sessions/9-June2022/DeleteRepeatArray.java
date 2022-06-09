7386da5baeba4eb1a1c1065c8ff472e3fabc163f
Suraj-Suryawanshi <74811909+Suraj-Suryawanshi@users.noreply.github.com>
2022-06-09 14:03:24 +0530

    Update DeleteRepeatArray.java

--- Lab-Sessions/9-June2022/DeleteRepeatArray.java
+++ Lab-Sessions/9-June2022/DeleteRepeatArray.java
@@ -1,20 +1,50 @@
-package LabSessions;
+package corejava;
 
-//class name should be "DeleteRepeatArray"
-//otherwise solution won't be accepted
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DeleteRepeatArray {
-
-	public static char[] k;
+	public static Character[] k;
+	public static void main(String[] args)throws Exception {
+		//creating object of scanner class 
+		Scanner sc =new Scanner(System.in);
+		//creating char array
+		char[] arr=new char[4];
+		System.out.println("Array before deleting duplicates");
+		//for traversing the array and taking input 
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.next().charAt(0);
+			System.out.println("a["+i+"] = "+arr[i]);		
+		}
+		System.out.println("Array before deleting duplicates");
+		System.out.println("Size: "+arr.length);	//using length property for array length
+		System.out.println("\nArray after deleting duplicates : ");
 	
-	public static void main(String[] args) throws Exception {
+		int size=deleteRepeats(arr);
+		//for printing array after deleting duplicates
+		for(int i=0;i<size;i++) {
+			System.out.println("a["+i+"] = "+k[i]);
+			
+		}
+		System.out.println("Final Size: "+k.length);	//for printing size after deleting duplicate
+		
 
-	/* enter your code here. read input from SIDIN. print output to SIDOUT*/
-		 Scanner sc=new Scanner(System.in);
-		 char[]arr=new char[4];
-		 
-		 System.out.println("Array before deleting duplicates:");
-		 
-		 for(int i=0;i<arr.length;i++) {
-			 arr[i]=sc.next().charAt(0);
-			 
-			 System.out.println("a["+i+"]="+arr[i]);		 }
+	}
+	
+	public static int deleteRepeats(char[] arr) {
+		ArrayList<Character>lis=new ArrayList<Character>();
+		for(int i=0;i<arr.length;i++) {
+			if(lis.contains(arr[i])) {
+				continue;
+			}
+			else{
+				lis.add(arr[i]);
+				
+			}
+		}
+		k=new Character[lis.size()];
+		k=lis.toArray(k);
+		return k.length;
+	}
+
+}
