ceab2b82bf0786b444762530a5a01d8b0b1a1375
Suraj-Suryawanshi <74811909+Suraj-Suryawanshi@users.noreply.github.com>
2022-06-16 21:10:20 +0530

    Update TriviaGame.java

--- Lab-Sessions/16-Jun2022/TriviaGame.java
+++ Lab-Sessions/16-Jun2022/TriviaGame.java
@@ -6,14 +6,8 @@ public class TriviaGame {
 	
 	//creating and initializing array
 	public static int[] points = {2,3,2,3,5};
-	//creating and initializing array
-	public static String[] answers = {
-			"LOTUS",
-			"TIGER",
-			"HOCKEY",
-			"DELHI",
-			"PEACOCK"
-	};
+	public static String[] answers = {"LOTUS","TIGER","HOCKEY","DELHI","PEACOCK"};
+	
 	//creating static trivialMehtod which takes 2 String parameters
 	public static int triviaMethod(String[] questions, String[] stt) {
 		Scanner sc = new Scanner(System.in);
@@ -22,9 +16,9 @@ public class TriviaGame {
 		for(int i=0;i<5;i++) {
 			System.out.println(questions[i]);
 			System.out.println(stt[i]);
-			if(stt[i].toUpperCase().equals(answers[i])) {	//converting to uppercase & comparing with answers
+			if(stt[i].toUpperCase().equals(answers[i])) {	//converting input string arr to uppercase & comparing with answers
 				System.out.println("Correct!");
-				point+=points[i];
+				point+=points[i];		
 				System.out.println("");
 			}
 			else {
@@ -34,7 +28,6 @@ public class TriviaGame {
 		}
 		return point;			
 	}
-	//creating main method
 	public static void main(String[] args) {
 		/*Enter your code here. Read input from STDIN. Print output to STDOUT */ 
 		int point;
@@ -46,17 +39,16 @@ public class TriviaGame {
 				"Question 3: What is the National Game of INDIA?",
 				"Question 4: What is the capital of INDIA ?",
 				"Question 5: What is the National Bird of INDIA ?",
-				
 		};
 		//for traversing through questions and taking input of each
 		for(int i=0;i<5;i++) {			
 			System.out.println(questions[i]);
 			stt[i]=sc.nextLine();			
 		}
-		point=triviaMethod(questions,stt);		//calling trivialMethod and storing in point
+		point=triviaMethod(questions,stt);		//calling trivialMethod and storing in int point
 		System.out.println("Final score: "+point);
 		
-		//for declaring rewards according to score
+		//for declaring rewards according to score/points
 		if(point==15) {
 			System.out.println("Congrats! You are the winner");
 		}
